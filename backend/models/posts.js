240bdc1e01f518f40cab6df31bd922e31c010d1a
matheus <mtava002@gmail.com>
2020-10-07 17:09:43 -0400

    barebone schema

---
+++ backend/models/posts.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema({
+    author: { type: String, required: true },
+    body: { type: String, required: true },
+    created: { type: Date, default: Date.now },
+    postId: { type: String, required: true },
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+    parentId: { type: String, default: null }
+
+
+});
+
+const postSchema = new Schema({
+    title: { type: String, required: true },
+    postType: { type: [String], enum: ['Collab', 'Critique', 'Kata'] },
+    author: { type: String, required: true },
+    body: { type: String, required: true },
+    created: { type: Date, default: Date.now },
+    author: { type: String, required: true },
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+    postId: { type: String, required: true },
+    comments: [commentSchema],
+    views: { type: Number, default: 0 },
+
+
+})
+
+module.exports = model("Post", postSchema);
+module.exports = model("Comment", commentSchema)
