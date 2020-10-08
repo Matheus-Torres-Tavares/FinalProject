85bcd86ad8b60949476bfc90ffcdd5325d7d439f
second login <secondary@Matthews-MacBook-Pro-2.local>
2020-10-08 11:14:38 -0400

    backend endpoints

---
+++ backend/routes/users.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+
+const User = require("../models/user.js");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const bio = req.body.bio;
+
+  const newUser = new User({ username, bio });
+
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
+module.exports = router;
