296d5ac0067caf75d7b032b2aba394fbbd9d2642
matheus <mtava002@gmail.com>
2020-10-08 17:28:18 -0400

    MFin' Jones

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,9 +1,11 @@
 import React, { Component, useState, useEffect } from 'react';
 import actions from '../../api/index'
+import TheContext from '../../TheContext'
 
 
 
 const Home = (props) => {
+  console.log(props)
   //   let [fruit, setFruit] = useState('kiwi')
 
   //   useEffect(()=>{
@@ -13,10 +15,14 @@ const Home = (props) => {
   //   const changeFruit = () => {
   //     setFruit("pineapple")
   //   }
+  let { user } = React.useContext(TheContext)
+  const [post, setPost] = useState({})
   const addingPosts = async () => {
 
     let res = await actions.addPost({ title: "ballsack, they are cool", username: "Motherfucker Jones", text: "hello hello", })
     console.log(res)
+    setPost(res.data.WereAddingApost)
+    console.log(post)
 
 
 
@@ -24,7 +30,9 @@ const Home = (props) => {
   return (
     <div>
       Home
+      {props.thePropUser?.name} {user?.name}
       <button onClick={addingPosts}>Add Post</button>
+      <p>{post.username}</p>
 
     </div>
   )
