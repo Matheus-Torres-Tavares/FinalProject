64edc02213341bc9f2287456b4dc840c5b49c1ab
Sebastian Grana <sebastiangrana1@gmail.com>
2020-10-08 12:35:03 -0400

    google signup fix

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,43 +1,12 @@
-// const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
 const PLM = require("passport-local-mongoose");
-
-// const userSchema = new Schema(
-//   {
-//     email: String,
-//     name: String,
-//     googleId: String,
-//     imageUrl: String,
-//   },
-//   {
-//     timestamps: true,
-//     versionKey: false,
-//   }
-// );
-
-// userSchema.plugin(PLM, { usernameField: "email" });
-
-// module.exports = model("User", userSchema);
-
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      googleId: String,
-      required: true,
-      trim: true,
-      minlength: 3,
-    },
-
-    bio: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 10,
-    },
-    email: String
+    email: String,
+    name: String,
+    googleId: String,
+    imageUrl: String,
   },
   {
     timestamps: true,
@@ -46,7 +15,33 @@ const userSchema = new Schema(
 );
 
 userSchema.plugin(PLM, { usernameField: "email" });
+module.exports = model("User", userSchema);
 
-const User = mongoose.model("User", userSchema);
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-module.exports = User;
+// const userSchema = new Schema(
+//   {
+//     username: {
+//       type: String,
+//       googleId: String,
+//       required: true,
+//       trim: true,
+//       minlength: 3,
+//     },
+
+//     bio: {
+//       type: String,
+//       trim: true,
+//       minlength: 10,
+//     },
+//     email: String
+//   },
+//   {
+//     timestamps: true,
+//     versionKey: false,
+//   }
+// );
+
+
+// module.exports = User;
