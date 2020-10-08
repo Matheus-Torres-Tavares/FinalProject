fdf304ff83662f2eb596f2cf7f83fce725c09459
second login <23matthewangel@gmail.com>
2020-10-08 11:14:38 -0400

    backend endpoints

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,42 @@
-const { Schema, model } = require("mongoose");
-const PLM = require("passport-local-mongoose");
+// const { Schema, model } = require("mongoose");
+// const PLM = require("passport-local-mongoose");
+
+// const userSchema = new Schema(
+//   {
+//     email: String,
+//     name: String,
+//     googleId: String,
+//     imageUrl: String,
+//   },
+//   {
+//     timestamps: true,
+//     versionKey: false,
+//   }
+// );
+
+// userSchema.plugin(PLM, { usernameField: "email" });
+
+// module.exports = model("User", userSchema);
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    email: String,
-    name: String,
-    googleId: String,
-    imageUrl: String,
+    username: {
+      type: String,
+      googleId: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+
+    bio: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10,
+    },
   },
   {
     timestamps: true,
@@ -14,6 +44,8 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.plugin(PLM, { usernameField: "email" });
+// userSchema.plugin(PLM, { usernameField: "email" });
 
-module.exports = model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- backend/models/posts.js
+++ backend/models/posts.js
@@ -1,32 +1,49 @@
-const { Schema, model } = require("mongoose");
+// const { Schema, model } = require("mongoose");
 
-const commentSchema = new Schema({
-    author: { type: String, required: true },
-    body: { type: String, required: true },
-    created: { type: Date, default: Date.now },
-    postId: { type: String, required: true },
-    upVotes: { type: Number, default: 0 },
-    downVotes: { type: Number, default: 0 },
-    parentId: { type: String, default: null }
+// const commentSchema = new Schema({
+//     author: { type: String, required: true },
+//     body: { type: String, required: true },
+//     created: { type: Date, default: Date.now },
+//     postId: { type: String, required: true },
+//     upVotes: { type: Number, default: 0 },
+//     downVotes: { type: Number, default: 0 },
+//     parentId: { type: String, default: null }
 
+// });
 
-});
+// const postSchema = new Schema({
+//     title: { type: String, required: true },
+//     postType: { type: [String], enum: ['Collab', 'Critique', 'Kata'] },
+//     author: { type: String, required: true },
+//     body: { type: String, required: true },
+//     created: { type: Date, default: Date.now },
+//     author: { type: String, required: true },
+//     upVotes: { type: Number, default: 0 },
+//     downVotes: { type: Number, default: 0 },
+//     postId: { type: String, required: true },
+//     comments: [commentSchema],
+//     views: { type: Number, default: 0 },
+
+// })
+
+// module.exports = model("Post", postSchema);
+// module.exports = model("Comment", commentSchema)
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    title: { type: String, required: true },
-    postType: { type: [String], enum: ['Collab', 'Critique', 'Kata'] },
-    author: { type: String, required: true },
-    body: { type: String, required: true },
-    created: { type: Date, default: Date.now },
-    author: { type: String, required: true },
-    upVotes: { type: Number, default: 0 },
-    downVotes: { type: Number, default: 0 },
-    postId: { type: String, required: true },
-    comments: [commentSchema],
-    views: { type: Number, default: 0 },
+  title: { type: String, required: true },
+  username: { type: String, required: true },
+  text: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  // comments: [commentSchema],
+});
 
+// const Comments = mongoose.model("Comment", commentSchema);
 
-})
+const Posts = mongoose.model("Post", postSchema);
 
-module.exports = model("Post", postSchema);
-module.exports = model("Comment", commentSchema)
+// module.exports = Comments;
+
+module.exports = Posts;
