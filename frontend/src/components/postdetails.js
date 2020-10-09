4e280cec1f1653617ee806545caa506148da0966
matheus <mtava002@gmail.com>
2020-10-09 17:13:40 -0400

    we love posts

--- frontend/src/components/postdetails.js
+++ frontend/src/components/postdetails.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import actions from '../../api/index'
 
 const PostDetails = (props) => {
     console.log(props)
+    // useEffect(() => {
+    //     async function getThePost() {
+    //         let res = await actions.get
+    //     } getThePost()
+    // }, [])
+
     return (
         <div>
             hello {props.match.params.id}
