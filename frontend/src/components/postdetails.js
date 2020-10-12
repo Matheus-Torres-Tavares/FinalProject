42760bfab87ed64ad35d71b220b0b631283765a4
matheus <mtava002@gmail.com>
2020-10-12 12:46:50 -0400

    light bootstrapping

--- frontend/src/components/postdetails.js
+++ frontend/src/components/postdetails.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import actions from '../api/index'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Card } from 'react-bootstrap'
 
 const PostDetails = (props) => {
     const [postDetails, setPostDetails] = useState([])
@@ -15,10 +17,22 @@ const PostDetails = (props) => {
 
     return (
         <div>
-            {postDetails.date}
-            {postDetails.title}
-            {postDetails.text}
+
+            <Card style={{ width: '35rem' }}>
+                <Card.Body>
+                    <Card.Title>{postDetails.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">By: {postDetails.username}</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Posted on :{postDetails.date} </Card.Subtitle>
+                    <Card.Text>
+                        {postDetails.text}
+                    </Card.Text>
+                    <Card.Link href="#">Github link </Card.Link>
+                    <Card.Link href="#"> Contact Me</Card.Link>
+                </Card.Body>
+            </Card>
         </div>
+
+
 
 
 
