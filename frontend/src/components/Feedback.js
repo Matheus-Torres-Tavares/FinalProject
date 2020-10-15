b8d50188f8264278f5a2a15213be2797b7ca3f2e
matheus <mtava002@gmail.com>
2020-10-15 16:44:58 -0400

    etf

--- frontend/src/components/Feedback.js
+++ frontend/src/components/Feedback.js
@@ -8,10 +8,6 @@ import { Link } from 'react-router-dom'
 import Comments from './Comments'
 import "./css/app.css"
 
-
-
-
-
 function Kata(props) {
     useEffect(() => {
         getKata()
@@ -27,18 +23,12 @@ function Kata(props) {
     console.log(kataList)
 
 
-
-
     const getKata = async () => {
         let res = await actions.getPosts({ type: "feedback" })
         console.log(res?.data)
         setKataList(res?.data.posts)
         console.log(res?.data.posts)
         console.log(kataList)
-
-
-
-
     }
 
     async function handleSubmit(e) {
@@ -58,12 +48,6 @@ function Kata(props) {
         getKata()
     }
 
-
-
-
-
-
-
     return (
 
         <div>
@@ -75,16 +59,11 @@ function Kata(props) {
                             <div className="form-group">
                                 <label><b>Project seeking feedback:</b></label>
                                 <input className="form-control" onChange={(e) => setTitle(e.target.value)} type="text" name="title">
-
                                 </input>
                             </div>
                             <div className="form-group">
-
                                 <label><b>Technologies used:</b></label>
-
-
                                 <input className="form-control" onChange={(e) => setTechnologies(e.target.value)} type="text" name="title">
-
                                 </input>
                             </div>
                             <div className="form-group">
@@ -107,8 +86,6 @@ function Kata(props) {
                 return (
                     <Card className="cardbody" style={{ width: '60rem', height: '10rem' }}>
                         <Card.Body >
-                            {/* <img src={post?.userID?.imageUrl} />  */}
-                            {/* await actions.vote({ type: "feedback", vote: -1, postId: kata._id })  */}
                             <Card.Title><Link to={`/feedback/${kata._id}`}><h3>{kata.title.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); })}</h3></Link></Card.Title>
                             <Card.Subtitle className="mb-2 text-muted">By: <img src={kata?.userID?.imageUrl} width="30px" height="30px" /> {kata.username}</Card.Subtitle>
                             <Card.Subtitle className="mb-2 text-muted">Posted on: {moment(kata.date).format("MMM Do YY")} </Card.Subtitle>
@@ -121,12 +98,8 @@ function Kata(props) {
                             }}>Delete</Button> : <></>}
                         </Card.Body>
                     </Card>
-
-
                 )
-
             })}
-
         </div>
     )
 }
