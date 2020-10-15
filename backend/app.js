b572149196d5a50139470ad69ecfeb561d9fca10
matheus <mtava002@gmail.com>
2020-10-15 15:05:20 -0400

    hello

--- backend/app.js
+++ backend/app.js
@@ -67,34 +67,4 @@ app.get('*', (req, res, next) => {
 
 module.exports = app;
 
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-// const app = express();
 
-// const port = process.env.PORT || 5000;
-
-// app.use(cors());
-
-// app.use(express.json());
-
-// const MONGODB_URI =
-//   process.env.MONGODB_URI || "mongodb://localhost/deploymentExample";
-// console.log("Connecting DB to ", MONGODB_URI);
-
-// mongoose
-//   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then((x) =>
-//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-//   )
-//   .catch((err) => console.error("Error connecting to mongo", err));
-
-// const postRouter = require("./routes/posts");
-// const userRouter = require("./routes/users");
-// app.use("/posts", postRouter);
-// app.use("/users", userRouter);
-
-// app.listen(port, () => {
-//   console.log(`listening on port: ${port}`);
-// });
